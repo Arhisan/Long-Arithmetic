3a3762648a4992e5d767c405058361dc184a106e
arhisan <pisaga@yandex.ru>
2017-12-23 22:41:39 +0300

    2 - From long to LongInt constructor

--- Long Arithmetic/Long Arithmetic/Program.cs
+++ Long Arithmetic/Long Arithmetic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Long_Arithmetic
 {
@@ -7,6 +8,44 @@ namespace Long_Arithmetic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+    }
+
+    class LongInt
+    {
+        private BitArray Bits;
+
+        public LongInt(int order = 1)
+        {
+            Bits = new BitArray(order);
+        }
+
+        public void SetValue(long val)
+        {
+            const int mask = 1;
+            string binarystr = "";
+            while (val > 0)
+            {
+                binarystr = (val & mask) + binarystr;
+                val = val >> 1;
+            }
+            Bits = new BitArray(binarystr.Length);
+            for (int i = 0; i < binarystr.Length; i++)
+            {
+                Bits[i] = binarystr[i] == '1';
+            }
+        }
+
+        public static LongInt operator +(LongInt a, LongInt b)
+        {
+            return a;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
