6e5c5e00f9fb002970f5e32574efa1b49cfdfb52
arhisan <pisaga@yandex.ru>
2018-01-03 23:29:30 +0800

    7 - LongInt -> LongIntBin & Decomposition

---
+++ Long Arithmetic/Long Arithmetic/LongInt.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+
+namespace Long_Arithmetic
+{
+    public class LongInt
+    {
+        
+    }
+
+    class LongIntBin : IComparable
+    {
+        private BitArray Bits;
+        public LongIntBin(long value = 0) => SetValue(value);
+        public void SetValue(long val)
+        {
+            const int mask = 1;
+            string binarystr = "";
+            while (val > 0)
+            {
+                binarystr += (val & mask); //+ binarystr;
+                val = val >> 1;
+            }
+            Bits = new BitArray(binarystr.Length);
+            for (int i = 0; i < binarystr.Length; i++)
+            {
+                Bits[i] = binarystr[i] == '1';
+            }
+        }
+
+        public static LongIntBin operator +(LongIntBin a, LongIntBin b)
+        {
+            int l = a.Bits.Length;
+            int r = b.Bits.Length;
+            LongIntBin res = new LongIntBin();
+            //Lenght of the result BitArray
+            int n = Math.Max(l, r) + 1;
+            res.Bits = new BitArray(n);
+            bool carry = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                //Automatical 0-filling and out-of-range prevention
+                int aval = (i < l ? (a.Bits[i] ? 1 : 0) : 0);
+                int bval = (i < r ? (b.Bits[i] ? 1 : 0) : 0);
+                int cval = (carry ? 1 : 0);
+
+                int value = aval + bval + cval;
+                switch (value)
+                {
+                    case 0: res.Bits[i] = false; carry = false; break;
+                    case 1: res.Bits[i] = true; carry = false; break;
+                    case 2: res.Bits[i] = false; carry = true; break;
+                    case 3: res.Bits[i] = true; carry = true; break;
+                }
+            }
+            return res;
+        }
+
+        public static LongIntBin operator -(LongIntBin a, LongIntBin b)
+        {
+            int l = a.Bits.Length;
+            int r = b.Bits.Length;
+            LongIntBin res = new LongIntBin();
+            //Lenght of the result BitArray
+            int n = Math.Max(l, r) + 1;
+            res.Bits = new BitArray(n);
+            bool carry = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                //Automatical 0-filling and out-of-range prevention
+                int aval = (i < l ? (a.Bits[i] ? 1 : 0) : 0);
+                int bval = (i < r ? (b.Bits[i] ? 1 : 0) : 0);
+                int cval = (carry ? 1 : 0);
+
+                int value = aval - bval - cval;
+                switch (value)
+                {
+                    case 1: res.Bits[i] = true; carry = false; break;
+                    case 0: res.Bits[i] = false; carry = false; break;
+                    case -1: res.Bits[i] = true; carry = true; break;
+                    case -2: res.Bits[i] = false; carry = true; break;
+                }
+            }
+            return res;
+        }
+
+        public static LongIntBin operator *(LongIntBin a, LongIntBin b)
+        {
+            return
+                Math.Max(a.Bits.Length, b.Bits.Length) > 256
+                ? KaratsubaMult(a, b)
+                : NaiveMult(a, b);
+        }
+
+        public static LongIntBin operator /(LongIntBin a, LongIntBin b)
+        {
+            return a;
+        }
+
+        public static LongIntBin operator %(LongIntBin a, LongIntBin b)
+        {
+            return a;
+        }
+
+        public static bool operator >(LongIntBin a, LongIntBin b) => a.CompareTo(b) > 0;
+        public static bool operator <(LongIntBin a, LongIntBin b) => a.CompareTo(b) < 0;
+        public static bool operator >=(LongIntBin a, LongIntBin b) => a.CompareTo(b) >= 0;
+        public static bool operator <=(LongIntBin a, LongIntBin b) => a.CompareTo(b) <= 0;
+        public static bool operator ==(LongIntBin a, LongIntBin b) => a.CompareTo(b) == 0;
+        public static bool operator !=(LongIntBin a, LongIntBin b) => a.CompareTo(b) != 0;
+
+
+        //Convert int to LongIntBin implicitly
+        public static implicit operator LongIntBin(int v) => new LongIntBin(v);
+
+        //Transform number to "Human form"
+        public override string ToString()
+        {
+            BigInteger sum = 0;
+            for (int i = 0; i < Bits.Length; i++)
+            {
+                sum += Bits[i] ? BigInteger.Pow(2, i) : 0;
+            }
+            return sum.ToString();
+        }
+
+        //O(n^log(3) ~ n^1.58)
+        private static LongIntBin KaratsubaMult(LongIntBin a, LongIntBin b)
+        {
+            return 0;
+        }
+
+        //Naive Multiplication alg - O(n^2)
+        private static LongIntBin NaiveMult(LongIntBin a, LongIntBin b)
+        {
+            int l = a.Bits.Length;
+            int r = b.Bits.Length;
+            LongIntBin res = new LongIntBin();
+            res.Bits = new BitArray(l + r);
+            bool carry = false;
+            for (int i = 0; i < l; i++)
+            {
+                carry = false;
+                for (int j = 0; j < r; j++)
+                {
+                    bool oldval = res.Bits[i + j];
+                    bool result = a.Bits[i] & b.Bits[j];
+                    res.Bits[i + j] ^= carry ^ result;
+                    carry = (oldval | carry) & (oldval | result) &
+                            (carry | result);
+                }
+                res.Bits[i + r] ^= carry;
+            }
+            return res;
+        }
+
+        //IComparable nonstatic implementation
+        public int CompareTo(object obj)
+        {
+            LongIntBin snd = (LongIntBin)obj;
+            if (Bits.Length > snd.Bits.Length)
+            {
+                return 1;
+            }
+            else if (Bits.Length < snd.Bits.Length)
+            {
+                return -1;
+            }
+            else
+            {
+                int maxlenght = Math.Max(Bits.Length, snd.Bits.Length);
+                for (int i = maxlenght - 1; i >= 0; i--)
+                {
+                    int aval = i >= Bits.Length ? 0 : (Bits[i] ? 1 : 0);
+                    int bval = i >= snd.Bits.Length ? 0 : (snd.Bits[i] ? 1 : 0);
+                    if (aval > bval)
+                        return 1;
+                    else if (aval < bval)
+                        return -1;
+                }
+                return 0;
+            }
+
+        }
+    }
+}
--- Long Arithmetic/Long Arithmetic/Program.cs
+++ Long Arithmetic/Long Arithmetic/Program.cs
@@ -8,15 +8,24 @@ namespace Long_Arithmetic
 {
     class Program
     {
+        private static LongIntBin ooo = 0;
         static void Main(string[] args)
         {
             StaticManualTest(8, 5);
+            StaticManualTest(5, 8);
+
             StaticManualTest(100, 6);
             StaticManualTest(3, 3);
+
+            for (int i = 1; i < 10001; i *= 10)
+            {
+                PerformanceTest(i);
+            }
+            WriteLine(ooo);
             Read();
         }
 
-        static void StaticManualTest(LongInt n1, LongInt n2)
+        static void StaticManualTest(LongIntBin n1, LongIntBin n2)
         {
             WriteLine("----------");
             WriteLine($"n1 = {n1}, n2 = {n2}");
@@ -31,160 +40,55 @@ namespace Long_Arithmetic
             WriteLine($"{n1} <= {n2} = {n1 <= n2}");
             WriteLine($"{n1} == {n2} = {n1 == n2}");
             WriteLine($"{n1} != {n2} = {n1 != n2}");
-        }
-    }
 
-    class LongInt: IComparable
-    {
-        private BitArray Bits;
-        public LongInt(long value=0) => SetValue(value);
-        public void SetValue(long val)
-        {
-            const int mask = 1;
-            string binarystr = "";
-            while (val > 0)
+            LongIntBin num = 1;
+            for (int i = 0; i < 200; i++)
             {
-                binarystr += (val & mask); //+ binarystr;
-                val = val >> 1;
+                num *= int.MaxValue;
             }
-            Bits = new BitArray(binarystr.Length);
-            for (int i = 0; i < binarystr.Length; i++)
-            {
-                Bits[i] = binarystr[i] == '1';
-            }
+            WriteLine($"Big Number {num}");
         }
 
-        public static LongInt operator +(LongInt a, LongInt b)
+        static void PerformanceTest(int n)
         {
-            int l = a.Bits.Length;
-            int r = b.Bits.Length;
-            LongInt res = new LongInt();
-            //Lenght of the result BitArray
-            int n = Math.Max(l, r) + 1;
-            res.Bits = new BitArray(n);
-            bool carry = false;
-
+            WriteLine($"---------   n = {n}");
+            DateTime start = DateTime.Now;
+            DateTime current = DateTime.Now;
             for (int i = 0; i < n; i++)
             {
-                //Automatical 0-filling and out-of-range prevention
-                int aval = (i < l ? (a.Bits[i] ? 1 : 0) : 0);
-                int bval = (i < r ? (b.Bits[i] ? 1 : 0) : 0);
-                int cval = (carry ? 1 : 0);
-                
-                int value = aval + bval + cval;
-                switch (value)
-                { 
-                    case 0: res.Bits[i] = false; carry = false; break;
-                    case 1: res.Bits[i] = true;  carry = false; break;
-                    case 2: res.Bits[i] = false; carry = true; break;
-                    case 3: res.Bits[i] = true;  carry = true; break;
-                }
-            }
-            return res;
-        }
-
-        public static LongInt operator -(LongInt a, LongInt b)
-        {
-            return a;
-        }
-
-        public static LongInt operator *(LongInt a, LongInt b)
-        {
-            return 
-                Math.Max(a.Bits.Length, b.Bits.Length) > 256
-                ? KaratsubaMult(a, b) 
-                : NaiveMult(a, b);
-        }
-
-        public static LongInt operator /(LongInt a, LongInt b)
-        {
-            return a;
-        }
-
-        public static LongInt operator %(LongInt a, LongInt b)
-        {
-            return a;
-        }
-
-        public static bool operator >(LongInt a, LongInt b) => a.CompareTo(b) > 0;
-        public static bool operator <(LongInt a, LongInt b) => a.CompareTo(b) < 0;
-        public static bool operator >=(LongInt a, LongInt b) => a.CompareTo(b) >= 0;
-        public static bool operator <=(LongInt a, LongInt b) => a.CompareTo(b) <= 0;
-        public static bool operator ==(LongInt a, LongInt b) => a.CompareTo(b) == 0;
-        public static bool operator !=(LongInt a, LongInt b) => a.CompareTo(b) != 0;
-
-
-        //Convert int to LongInt implicitly
-        public static implicit operator LongInt(int v) => new LongInt(v);
-
-        //Transform number to "Human form"
-        public override string ToString()
-        {
-            BigInteger sum = 0;
-            for (int i = 0; i < Bits.Length; i++)
-            {
-                sum += Bits[i] ? BigInteger.Pow(2, i) : 0;
-            }
-            return sum.ToString();
-        }
-
-        //O(n^log(3) ~ n^1.58)
-        private static LongInt KaratsubaMult(LongInt a, LongInt b)
-        {
-            return 0;
-        }
-
-        //Naive Multiplication alg - O(n^2)
-        private static LongInt NaiveMult(LongInt a, LongInt b)
-        {
-            int l = a.Bits.Length;
-            int r = b.Bits.Length;
-            LongInt res = new LongInt();
-            res.Bits = new BitArray(l + r);
-            bool carry = false;
-            for (int i = 0; i < l; i++)
-            {
-                carry = false;
-                for (int j = 0; j < r; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    bool oldval = res.Bits[i + j];
-                    bool result = a.Bits[i] & b.Bits[j];
-                    res.Bits[i + j] ^= carry ^ result;
-                    carry = (oldval | carry) & (oldval | result) &
-                            (carry | result); 
+                    LongIntBin a = i;
+                    LongIntBin b = j;
+                    LongIntBin c = a + b;
                 }
-                res.Bits[i + r] ^= carry;
             }
-            return res;
-        }
-
-        //IComparable nonstatic implementation
-        public int CompareTo(object obj)
-        {
-            LongInt snd = (LongInt)obj;
-            if (Bits.Length > snd.Bits.Length)
+            Console.WriteLine($"Add time {DateTime.Now - current}");
+            current = DateTime.Now;
+            for (int i = 0; i < n; i++)
             {
-                return 1;
-            }
-            else if (Bits.Length < snd.Bits.Length)
-            {
-                return -1;
-            }
-            else
-            {
-                int maxlenght = Math.Max(Bits.Length, snd.Bits.Length);
-                for (int i = maxlenght - 1; i >= 0; i--)
+                for (int j = 0; j < n; j++)
                 {
-                    int aval = i >= Bits.Length ? 0 : (Bits[i] ? 1 : 0);
-                    int bval = i >= snd.Bits.Length ? 0: (snd.Bits[i] ? 1 : 0);
-                    if (aval > bval)
-                        return 1;
-                    else if (aval < bval)
-                        return -1;
+                    LongIntBin a = i;
+                    LongIntBin b = j;
+                    LongIntBin c = a * b;
                 }
-                return 0;
             }
-
+            Console.WriteLine($"Mult time {DateTime.Now - current}");
+            current = DateTime.Now;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    LongIntBin a = i;
+                    LongIntBin b = j;
+                    LongIntBin c = a - b;
+                    
+                }
+            }
+            Console.WriteLine($"Substr time {DateTime.Now - current}");
+            current = DateTime.Now;
+            WriteLine($"Total {current - start}");
         }
-    }
+    } 
 }
